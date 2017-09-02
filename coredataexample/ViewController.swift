5c71b425571e3a26acfeb869c071881c193a6abf
Muhammed Esad Kaya <mesadkaya@Muhammed-MacBook-Pro.local>
2017-09-02 12:45:06 +0300

    simple core data example. first commit.

--- coredataexample/ViewController.swift
+++ coredataexample/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName:"Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do
+        {
+            let results = try context.fetch(request)
+            
+            if results.count > 0
+            {
+                for result in results as! [NSManagedObject]
+                {
+                    if let email = result.value(forKey: "email") as? String
+                    {
+                        print("****** email: \(email)")
+                    }
+                }
+            }
+        }
+        catch
+        {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +51,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func registerButton(_ sender: Any)
+    {
+  
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+
+        let createUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        createUser.setValue(emailField.text, forKey: "email")
+        createUser.setValue(passwordField.text, forKey: "password")
+        
+        do
+        {
+           try context.save()
+            print("******** grats **********")
+        }
+        catch
+        {
+            print(error)
+        }
+        
+    }
 
 }
 
